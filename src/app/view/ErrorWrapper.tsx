b21ece33cc9c2bf420de1f156367eb4a4f9602cb
Dick Fickling <dick@fickling.us>
2024-02-11 09:49:26 -0500

    send people from fickling to uaul

--- src/app/view/ErrorWrapper.tsx
+++ src/app/view/ErrorWrapper.tsx
@@ -28,6 +28,18 @@ export default function ErrorWrapper({
       <h1 className="mb-4 text-2xl font-bold">Error</h1>
       <p>There was an error fetching the upgrade list.</p>
       <div className="text-xs">{JSON.stringify(error, null, 2)}</div>
+      {location.host === "uaul.fickling.us" && (
+        <div className="mt-2">
+          Please{" "}
+          <a
+            className="text-blue-800 underline"
+            href={`https://uaul.d10g.co${location.pathname}${location.search}`}
+          >
+            click here
+          </a>{" "}
+          to try again.
+        </div>
+      )}
     </main>
   );
 }
