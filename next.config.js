8cc453f21b44f1f1a2350b5b304ac48a06b9ae22
Dick Fickling <dick@fickling.us>
2024-02-11 10:05:38 -0500

    always redirect to d10g

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      // if the host is `example.com`,
+      // this redirect will be applied
+      {
+        source: "/:path",
+        has: [
+          {
+            type: "host",
+            value: "uaul.fickling.us",
+          },
+        ],
+        permanent: false,
+        destination: "https://uaul.d10g.co/:path",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
